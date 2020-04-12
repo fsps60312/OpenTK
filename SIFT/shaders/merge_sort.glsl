15c54ba50ea186941cde288c291e5521adfcecc6
fsps60312 <fsps60312@yahoo.com.tw>
2020-04-12 22:16:28 +0800

    parallel merge sort: clean code

--- SIFT/shaders/merge_sort.glsl
+++ SIFT/shaders/merge_sort.glsl
@@ -14,7 +14,6 @@ int search(const in int offset_a, const in int n_a, const in int offset_b, const
 	while (a_l < a_r) {
 		const int a = (a_l + a_r) >> 1;
 		const int b = a_plus_b - a;
-		// a up if: s_a[a] <= s_b[b - 1]
 		if (b - 1 >= 0 && buf_s[offset_a + a] <= buf_s[offset_b + b - 1]) a_l = a + 1;
 		else a_r = a;
 	}
@@ -23,25 +22,21 @@ int search(const in int offset_a, const in int n_a, const in int offset_b, const
 
 int merge2(const in int offset, const in int i, const in int n) {
 	const int n_a = 1 << (level - 1);
-	if (n_a >= n) return buf_s[offset + i];
+	if (n_a >= n) return offset + i;
 	const int n_b = n - n_a;
-//	buf_debug[offset + i] = 0;
-	const int a = search(offset, n_a, offset + n_a, n - n_a, i); // b1 = i - a1
+	const int a = search(offset, n_a, offset + n_a, n_b, i); // b1 = i - a1
 	const int b = i - a;
-//	buf_debug[offset + i] = 0;
-	return a < n_a && (b >= n_b || buf_s[offset + a] <= buf_s[offset + n_a + b]) ? buf_s[offset + a] : buf_s[offset + n_a + b];
-//	buf_debug[offset + i] = i * 100 + a1*10 + a2;
+	return a < n_a && (b >= n_b || buf_s[offset + a] <= buf_s[offset + n_a + b]) ? offset + a : offset + n_a + b;
 }
 
-void merge(const in int offset, const in int i, const in int n) { // offset >> offset + i >> offset + n
-//	if (((i >> (level - 1)) & 1) != 1) return;
+int merge(const in int offset, const in int i, const in int n) { // offset >> offset + i >> offset + n
 	const int left_bound = i >> level << level;
-	buf_o[offset + i] = merge2(offset + left_bound, i - left_bound, min(n - left_bound, 1 << level));
+	return merge2(offset + left_bound, i - left_bound, min(n - left_bound, 1 << level));
 }
 
 void main() {
 	// Get Index in Global Work Group
 	const int i = int(global_invocation_id_x_offset + gl_GlobalInvocationID.x);
 	if (i >= buf_s.length()) return;
-	merge(buf_l[i], i - buf_l[i], buf_r[i] - buf_l[i] + 1);
+	buf_o[i] = buf_s[merge(buf_l[i], i - buf_l[i], buf_r[i] - buf_l[i] + 1)];
 }
